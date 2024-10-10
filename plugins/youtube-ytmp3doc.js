bae312844482bd33a431ef704a8a0018d52c9c43
JuancaOrtiz09 <juancataor@gmail.com>
2024-10-10 18:03:23 -0500

    Update youtube-ytmp3doc.js

--- plugins/youtube-ytmp3doc.js
+++ plugins/youtube-ytmp3doc.js
@@ -1,5 +1,8 @@
-import Starlights from '@StarlightsTeam/Scraper'
-import fetch from 'node-fetch' 
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
+import fetch from 'node-fetch'
+import yts from 'yt-search'
+import ytdl from 'ytdl-core'
+import axios from 'axios'
 let limit = 200
 
 let handler = async (m, { conn: star, args, text, isPrems, isOwner, usedPrefix, command }) => {
