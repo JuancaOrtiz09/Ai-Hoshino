a9524894996ced7c6129446d83fbf8b438365823
JuancaOrtiz09 <juancataor@gmail.com>
2024-10-10 15:15:37 -0500

    Update serbot-bots.js

--- plugins/serbot-bots.js
+++ plugins/serbot-bots.js
@@ -8,7 +8,7 @@ global.conns.forEach((conn) => {
 if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
 uniqueUsers.set(conn.user.jid.replace(/[^0-9]/g, ''), conn.user)}})
 
-const message = Array.from(uniqueUsers.values()).map((user, index) => `┌  ✩  *${index + 1}* : @${user.jid.replace(/[^0-9]/g, '')}\n│  ✩  *Link* : http://wa.me/${user.jid.replace(/[^0-9]/g, '')}\n└  ✩  *Nombre* : ${user.name || 'Ai Hoshino'}\n`
+const message = Array.from(uniqueUsers.values()).map((user, index) => `┌  ✩  *${index + 1}* : @${user.jid.replace(/[^0-9]/g, '')}\n│  ✩  *Link* : http://wa.me/${user.jid.replace(/[^0-9]/g, '')}\n└  ✩  *Nombre* : ${user.name || 'JuancaBot'}\n`
   ).join('\n')
   
 const replyMessage = message.length === 0 ? "" : message
@@ -19,7 +19,7 @@ let img = await (await fetch(`https://i.ibb.co/1dW0kGf/file.jpg`)).buffer()
 
 await _envio.sendFile(m.chat, img, 'thumbnail.jpg', responseMessage, m, false, { mentions: _envio.parseMention(responseMessage) })
 }
-handler.command = ['listjadibot', 'bots']
+handler.command = ['listJuancabot', 'bots']
 handler.help = ['bots']
 handler.tags = ['serbot']
 export default handler
